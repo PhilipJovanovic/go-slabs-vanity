b116bcaff87f094e005d047a330bcfd79b8c4c84
Philip Jovanovic <philip119@gmx.de>
2023-11-29 15:17:45 +0100

    upsi

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const BASE_URL = 'go.philip.id';
+export const BASE_URL = 'go.slabs.io';
 export const GITHUB_URL = 'github.com/sn0wlabs';
